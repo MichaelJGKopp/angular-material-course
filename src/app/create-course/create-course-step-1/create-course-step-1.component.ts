4e9e4d83462792322b5bcd47cc3ff91253995f3a
Michael Kopp <michaeljg.kopp@gmail.com>
2025-08-21 00:10:05 +0200

    Enhance course step 1 form with datepicker, checkbox and textfield

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
+import { MatCalendarCellClassFunction } from '@angular/material/datepicker';
 
-
+const LOREM_IPSUM = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 @Component({
     selector: "create-course-step-1",
     templateUrl: "create-course-step-1.component.html",
@@ -16,12 +17,21 @@ export class CreateCourseStep1Component {
       Validators.minLength(5),
       Validators.maxLength(60)
     ]],
-    releasedAt: [new Date(), Validators.required],
+    releasedAt: [new Date(1990,0,1), Validators.required],
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [LOREM_IPSUM, [Validators.required, Validators.minLength(3)]]
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+    if (view == 'month') {
+      return (date == 1) ? 'highlight-date' : '';
+    }
+
+    return '';
+  }
 
   constructor(private fb: UntypedFormBuilder) {
 
