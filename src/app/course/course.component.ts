37bbba4ff1b51771609208eebfe3bb101ee64b16
Michael Kopp <michaeljg.kopp@gmail.com>
2025-08-22 20:44:31 +0200

    add matSort

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,65 +1,85 @@
-import {AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  viewChild,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
-import {Course} from "../model/course";
-import {CoursesService} from "../services/courses.service";
-import {debounceTime, distinctUntilChanged, startWith, tap, delay, catchError, finalize} from 'rxjs/operators';
-import {merge, fromEvent, of, throwError} from "rxjs";
-import { Lesson } from '../model/lesson';
-
+import { Course } from "../model/course";
+import { CoursesService } from "../services/courses.service";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  startWith,
+  tap,
+  delay,
+  catchError,
+  finalize,
+} from "rxjs/operators";
+import { merge, fromEvent, of, throwError } from "rxjs";
+import { Lesson } from "../model/lesson";
 
 @Component({
-    selector: 'course',
-    templateUrl: './course.component.html',
-    styleUrls: ['./course.component.scss'],
-    standalone: false
+  selector: "course",
+  templateUrl: "./course.component.html",
+  styleUrls: ["./course.component.scss"],
+  standalone: false,
 })
 export class CourseComponent implements OnInit, AfterViewInit {
+  course: Course;
+  lessons: Lesson[] = [];
+  loading = false;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
-    course: Course;
-    lessons: Lesson[] = [];
-    loading = false;
-    @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(
+    private route: ActivatedRoute,
+    private coursesService: CoursesService
+  ) {}
 
-    constructor(private route: ActivatedRoute,
-                private coursesService: CoursesService) {
+  displayedColumns = ["seqNo", "description", "duration"];
 
-    }
+  ngOnInit() {
+    this.course = this.route.snapshot.data["course"];
+    this.loadLessonsPage();
+  }
 
-    displayedColumns = ['seqNo', 'description', 'duration'];
-
-    ngOnInit() {
-        this.course = this.route.snapshot.data["course"];
-        this.loadLessonsPage();
-    }
-
-    loadLessonsPage() {
-      this.loading = true;
-      this.coursesService.findLessons(this.course.id, 
-        "asc", 
-        this.paginator?.pageIndex ?? 0, 
-        this.paginator?.pageSize ?? 3)
+  loadLessonsPage() {
+    this.loading = true;
+    this.coursesService
+      .findLessons(
+        this.course.id,
+        this.sort?.direction ?? "asc",
+        this.paginator?.pageIndex ?? 0,
+        this.paginator?.pageSize ?? 3,
+        this.sort?.active ?? "seqNo",
+      )
       .pipe(
-        tap((lessons) => this.lessons = lessons),
+        tap((lessons) => (this.lessons = lessons)),
         catchError((error) => {
-          console.error('Error loading lessons', error);
+          console.error("Error loading lessons", error);
           alert("Error loading lessons");
           this.loading = false;
           return throwError(error);
         }),
-        finalize(() => this.loading = false)
+        finalize(() => (this.loading = false))
       )
       .subscribe();
-    }
+  }
 
-    
-    ngAfterViewInit() {
+  ngAfterViewInit() {
+    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+
+    merge(
+      this.sort.sortChange,
       this.paginator.page
-        .pipe(
-          tap(() => this.loadLessonsPage())
-        )
-        .subscribe();
-    }
+    ).pipe(
+      tap(() => this.loadLessonsPage())
+    ).subscribe();
+  }
 }
