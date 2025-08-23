f22f525ed863b785488697439b40a3f46de6edaa
Michael Kopp <michaeljg.kopp@gmail.com>
2025-08-23 19:46:52 +0200

    open NG MatDialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,25 +1,60 @@
-import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
-import {MAT_DIALOG_DATA,  MatDialog,  MatDialogConfig,  MatDialogRef} from '@angular/material/dialog';
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import * as moment from 'moment';
+import { Component, Inject, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogConfig,
+  MatDialogRef,
+} from "@angular/material/dialog";
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import * as moment from "moment";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css'],
-    standalone: false
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
+  standalone: false,
 })
 export class CourseDialogComponent implements OnInit {
+  description: string;
+  form = this.fb.group({
+    description: [this.course.description, Validators.required],
+    category: [this.course.category, Validators.required],
+    releasedAt: [new Date(), Validators.required],
+    longDescription: [this.course.longDescription, Validators.required],
+  });
 
-    constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) private course: Course,
+    private dialogRef: MatDialogRef<CourseDialogComponent>
+  ) {
+    this.description = course.description;
+  }
 
+  ngOnInit() {}
 
+  onCancel() {
+    this.dialogRef.close();
+  }
+
+  onSave() {
+    if (this.form.valid) {
+      this.dialogRef.close(this.form.value);
     }
-
-    ngOnInit() {
-
-    }
-
+  }
 }
 
+export function openEditCourseDialog(dialog: MatDialog, course: Course) {
+  const dialogRef = dialog.open(CourseDialogComponent, {
+    disableClose: true,
+    autoFocus: true,
+    // minHeight: "400px",
+    // minWidth: "600px",
+    // maxHeight: "80%",
+    // maxWidth: "80%",
+    data: { ...course },
+  });
+
+  return dialogRef.afterClosed();
+}
