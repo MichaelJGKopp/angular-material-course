6d05325c1e4d8d353b533ae5d727113e98b16ace
Michael Kopp <michaeljg.kopp@gmail.com>
2025-08-24 15:19:47 +0200

    feat: add basic responsive design with ng mat

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,50 +1,64 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
-import {Course} from "../model/course";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
+import { Course } from "../model/course";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
-import { filter } from 'rxjs/operators';
+import { openEditCourseDialog } from "../course-dialog/course-dialog.component";
+import { filter } from "rxjs/operators";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
 
 @Component({
-    selector: 'courses-card-list',
-    templateUrl: './courses-card-list.component.html',
-    styleUrls: ['./courses-card-list.component.scss'],
-    standalone: false
+  selector: "courses-card-list",
+  templateUrl: "./courses-card-list.component.html",
+  styleUrls: ["./courses-card-list.component.scss"],
+  standalone: false,
 })
 export class CoursesCardListComponent implements OnInit {
+  @Input()
+  courses: Course[];
+  cols: number = 3;
+  rowHeight: string = "500px";
 
-    @Input()
-    courses: Course[];
+  constructor(
+    private dialog: MatDialog,
+    private responsive: BreakpointObserver
+  ) {}
 
-    constructor(private dialog: MatDialog) {
-    }
+  ngOnInit() {
+    this.responsive
+      .observe([
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.HandsetLandscape,
+      ])
+      .subscribe((result) => {
+        this.cols = 3;
+        this.rowHeight = "500px";
 
-    ngOnInit() {
+        const breakpoints = result.breakpoints;
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+          this.cols = 1;
+        } else if (breakpoints[Breakpoints.TabletLandscape]) {
+          this.cols = 2;
+        } else if (breakpoints[Breakpoints.HandsetPortrait]) {
+          this.cols = 1;
+          this.rowHeight = "430px";
+        } else if (breakpoints[Breakpoints.HandsetLandscape]) {
+          this.cols = 1;
+        }
+      });
+  }
 
-    }
-
-    editCourse(course: Course) {
-        const dialog$ = openEditCourseDialog(this.dialog, course);
-        dialog$.pipe(
-            filter((val) => !!val)
-        )
-        .subscribe((result) => {
-            if (result) {
-                console.log('Course updated:', result);
-                // Update the course in the list
-                const index = this.courses.findIndex(c => c.id === result.id);
-                if (index !== -1) {
-                    this.courses[index] = result;
-                }
-            }
-        });
-    }
+  editCourse(course: Course) {
+    const dialog$ = openEditCourseDialog(this.dialog, course);
+    dialog$.pipe(filter((val) => !!val)).subscribe((result) => {
+      if (result) {
+        console.log("Course updated:", result);
+        // Update the course in the list
+        const index = this.courses.findIndex((c) => c.id === result.id);
+        if (index !== -1) {
+          this.courses[index] = result;
+        }
+      }
+    });
+  }
 }
-
-
-
-
-
-
-
-
-
